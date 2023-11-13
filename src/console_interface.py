823df125a668ff5ef7d1d36602b2e3c75b8c7776
LikeNoru <stkjas4657@naver.com>
2023-11-14 00:48:02 +0900

    first commit

---
+++ src/console_interface.py
@@ -0,0 +1,7 @@
+# noinspection PyMethodMayBeStatic
+class ConsoleInterface:
+    def __init__(self):
+        pass
+
+    def print_menu(self):
+        print("Hello Cloud!")
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from console_interface import ConsoleInterface
+
+
+def init():
+    c = ConsoleInterface()
+    c.print_menu()
+
+
+if __name__ == "__main__":
+    init()
